2c000b05564ee966a6f25076918c1ee2ddcf38a3
rafaerupng <rafaeru.png@gmail.com>
2025-04-01 14:32:05 -0300

    Testando API

--- netlify/functions/chatgpt.js
+++ netlify/functions/chatgpt.js
@@ -6,7 +6,7 @@ export async function handler(event) {
 
         const { input } = JSON.parse(event.body);
         
-        const response = await fetch("https://api-inference.huggingface.co/models/distilgpt2", {
+        const response = await fetch("https://api-inference.huggingface.co/models/gpt2", {
             method: "POST",
             headers: {
                 "Authorization": `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
